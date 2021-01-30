f83ad0b07ad5eed555f8a0e64efa86fc72e32826
Harold Gao <xiangaoo@163.com>
2021-01-30 11:51:33 +0800

    add beautiful prompt interface using tty-prompt

--- index.rb
+++ index.rb
@@ -1,53 +1,65 @@
 require_relative "spelling_bee_solver"
 require "set"
+require "tty-prompt"
+
+prompt = TTY::Prompt.new()
 
 puts "You can find a puzzle here: https://www.nytimes.com/puzzles/spelling-bee"
-puts <<DOC
-Choose a solver:
-[1] pangram solver(default)
-[2] fixed length solver
-[3] all length solver
-DOC
-begin
-  print "\n[1~3]: "
-  input = gets.chomp
-  if input == "" 
-    solver_num = 1
-  else
-    solver_num = input.to_i
-  end
-end until (1..3).include?(solver_num)
-puts "solver[#{solver_num}]"
+puts "---\n"
 
-begin
-  print "core letter:(e.g. =>a) "
-  core = gets.chomp.downcase.gsub(/[^a-z]/, "")[0]
-end while core.nil?
-puts "core letter is \"#{core}\""
-  
-begin
-  print "other letters:(e.g. =>bcde) "
-  others = gets.chomp.downcase.gsub(/#{core}|[^a-z]/, "")
-end while others.empty?
-others = others.chars.to_set.to_a
-puts "other letters are \"#{others.join}\""
+# get arguments
+# --------------------------------
+selections = {
+  "[1] pangram solver(default)": 1,
+  "[2] fixed length solver": 2,
+  "[3] all length solver": 3}
+solver_num = prompt.select("Choose a solver:", selections, cycle: true)
 
+core = prompt.ask("Core letter:") do |q|
+  q.required true
+  q.validate /\A[a-zA-Z]\Z/, "Core letter is only a english letter: a~z or A~Z"
+  q.modify :remove, :down
+end
+
+others = prompt.ask("Other letters:") do |q|
+  q.required true
+  q.validate /\A[a-zA-Z]{3,}\Z/, "Other letters are at least 3 english letters: a~z or A~Z"
+  q.modify :remove, :down
+end
+others = others.chars.to_set.delete(core).to_a
+print "Other letters are trimed to: "
+prompt.ok(others.join)
+
+prompt.say "...\n...\n..."
+prompt.say("If you have wait for too much time, you can enter [ctrl-C] to quit; and then choose a smaller WORD_MAX_LENGTH to retry", color: [:cyan, :dim])
+prompt.say "...\n...\n..."
+result = nil
+
+# solve
+# --------------------------------
 solver = SpellingBeeSolver.new
-puts "solved words are:"
-puts "If you have wait for too much time, you can enter [ctrl-C] to quit; and then choose a smaller WORD_MAX_LENGTH to retry"
 case solver_num
 when 2
   begin
     print "word length:[2~20] "
     word_len = gets.chomp.to_i
   end until (2..20).include?(word_len)
-  puts solver.solve_fixed_length(core, others, word_len)
+  result = solver.solve_fixed_length(core, others, word_len)
 when 1
-  puts solver.solve_pangram(core, others)
+  result = solver.solve_pangram(core, others)
 when 3
-  puts solver.solve(core, others)
+  result = solver.solve(core, others)
 else
-  puts "error with solver[#{solver_num}]"
+  prompt.error "error with solver[#{solver_num}]"
 end
 
-
+# print result
+# --------------------------------
+if not result.nil?
+  if result.size > 0
+    prompt.say("solved words are:") 
+    result.each { |word| prompt.ok(word) }
+  else
+    prompt.warn("no word found")
+  end
+end
